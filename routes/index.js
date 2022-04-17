8e96f586d784f7cf82b60f5860fd9c21fa91c937
VinitPurohit <vinpurohit98@gmail.com>
2022-04-16 23:07:12 -0400

    index and movie

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,5 @@
 /*
-File name: routes/index.js
-Author's name: Lau, Wing Yan
-StudentID: 301229696
-Web App: COMP229002-W2022-Midterm-301229696
+StudentID-301170869
 */
 
 var express = require('express');
